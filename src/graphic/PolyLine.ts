e275d8f833f5cca874627cc24a42e01254a6b5a6
JasonNee <jasonnee250@163.com>
2024-04-11 00:25:49 +0800

    fix： 更新语法错误

--- src/graphic/PolyLine.ts
+++ src/graphic/PolyLine.ts
@@ -1,23 +1,21 @@
-
 //折线
-import {IGraphicLine} from "src/graphic/IGraphicLine";
-import {Point} from "src/basic/Point";
-import {RectNode} from "src/basic/RectNode";
-import {GraphicUtils} from "@/utils";
+import { IGraphicLine } from "@/graphic/IGraphicLine";
+import { Point } from "@/basic/Point";
+import { RectNode } from "@/basic/RectNode";
+import { GraphicUtils } from "@/utils";
 
 export class PolyLine extends IGraphicLine {
-    points: Point[] = [];
+  points: Point[] = [];
 
-    draw(): void {
-    };
+  draw(): void {}
 
-    getTreeNode(): RectNode {
-        const bounds = GraphicUtils.getBoundsByPoints(this.points);
-        bounds.minX = bounds.minX - this.width;
-        bounds.minY = bounds.minY - this.width;
-        bounds.maxX = bounds.maxX + 2 * this.width;
-        bounds.maxY = bounds.maxY + 2 * this.width;
-        bounds.id = this.id;
-        return bounds;
-    }
+  getTreeNode(): RectNode {
+    const bounds = GraphicUtils.getBoundsByPoints(this.points);
+    bounds.minX = bounds.minX - this.width;
+    bounds.minY = bounds.minY - this.width;
+    bounds.maxX = bounds.maxX + 2 * this.width;
+    bounds.maxY = bounds.maxY + 2 * this.width;
+    bounds.id = this.id;
+    return bounds;
+  }
 }
