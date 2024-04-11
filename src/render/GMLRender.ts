76982c3475e16106654650376f0f894e273f789b
JasonNee <jasonnee250@163.com>
2024-04-11 20:32:06 +0800

    fix: 更新版本

--- src/render/GMLRender.ts
+++ src/render/GMLRender.ts
@@ -1,23 +1,32 @@
-import {IGraphicLine} from "@/graphic/IGraphicLine";
-import {GraphicNode} from "@/graphic/GraphicNode";
-import {GraphLinkLine} from "@/graphic/GraphicLinkLine";
+import { IGraphicLine } from "@/graphic/IGraphicLine";
+import { GraphicNode } from "@/graphic/GraphicNode";
+import { GraphLinkLine } from "@/graphic/GraphicLinkLine";
+import { RectNode } from "@/basic";
+import { IGraphicElement } from "@/graphic";
 
 export interface GMLData {
-    lineMap: Map<string, IGraphicLine>;
-    linkMap: Map<string, GraphLinkLine>;
-    nodeMap: Map<string, GraphicNode>;
+  lineMap: Map<string, IGraphicLine>;
+  linkMap: Map<string, GraphLinkLine>;
+  nodeMap: Map<string, GraphicNode>;
 }
 
-export interface GMLRender{
-    canvas:HTMLElement|null;
-    init(element: HTMLCanvasElement):void;
-    reset():void;
-    parse2GMLData(text:string):GMLData;
-    draw(text:string):void;
-    drawData(nodeMap: Map<string, GraphicNode>, lineMap: Map<string, IGraphicLine>):void;
-    layoutLine(nodeMap:Map<string,GraphicNode>, linkLine:GraphLinkLine):IGraphicLine|null;
+export interface GMLRender {
+  canvas: HTMLElement | null;
+  init(element: HTMLCanvasElement): void;
+  reset(): void;
+  parse2GMLData(text: string): GMLData;
+  draw(text: string): void;
+  drawData(
+    nodeMap: Map<string, GraphicNode>,
+    lineMap: Map<string, IGraphicLine>,
+  ): void;
+  layoutLine(
+    nodeMap: Map<string, GraphicNode>,
+    linkLine: GraphLinkLine,
+  ): IGraphicLine | null;
 
-    scale(sx:number,sy:number):void;
-    translation(dx:number,dy:number):void;
-    resetTransform():void;
+  scale(sx: number, sy: number): void;
+  translation(dx: number, dy: number): void;
+  resetTransform(): void;
+  dirtyDraw(bounds: RectNode, graphicList: IGraphicElement[]): void;
 }
